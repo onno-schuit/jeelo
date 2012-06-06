ed075ffa6a818de8f898acb98238db9a7ad8b862
William van Veldhoven <william@solin.nl>
2012-06-06 09:57:27 +0200

    Local configs

--- jeelo_cron/config.william.php
+++ jeelo_cron/config.william.php
@@ -8,6 +8,6 @@ $cs_dbname = 'jeelo_buffer';
 
 $cs_apache_conf_dir = '/etc/apache2/conf/jeelos';
 
-$cs_server_url = 'http://localhost/jeelo/local/cs_scripts/server.php';
+$cs_server_url = 'http://jeelo.debian.local/local/cs_scripts/server.php';
 $cs_jeelos_target_folder = '/home/jeelos';
 $cs_client_log_file = '/var/log/jeelo/client.txt';
---
+++ public_html/local/cs_scripts/config.william.php
@@ -0,0 +1,9 @@
+<?php
+
+# set these to your own needs
+global $cs_dbhost, $cs_dbname, $cs_dbpass, $cs_dbuser;
+$cs_dbhost = 'localhost';
+$cs_dbuser = 'root';
+$cs_dbpass = '';
+$cs_dbname = 'jeelo_buffer';
+
