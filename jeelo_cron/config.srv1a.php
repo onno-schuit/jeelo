6add3db11211c87227462153fad7ec2d8ff58d06
Testserver jeelo <info@solin.nl>
2012-06-07 14:08:56 +0200

    Added config for srv1a

---
+++ jeelo_cron/config.srv1a.php
@@ -0,0 +1,17 @@
+<?php
+
+# set these to your own needs
+$cs_dbhost = 'localhost';
+$cs_dbuser = 'root';
+$cs_dbpass = 'w29vizav';
+$cs_dbname = 'jeelo_buffer';
+
+$cs_apache_conf_dir = '/etc/httpd/conf/jeelos';
+
+$cs_server_url = 'http://moeder.srv1a.jeelo.nl/local/cs_scripts/server.php';
+$cs_jeelos_target_folder = '/home/jeelos';
+$cs_client_log_file = '/var/log/jeelo/client.txt';
+
+$mysql_dump_bin = '/usr/bin/mysqldump';
+$cs_apache_user = 'apache';
+
