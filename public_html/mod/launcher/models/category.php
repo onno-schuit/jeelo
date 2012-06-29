cd3c6b3364e2e6de6048e74c934e6ded40c55ed6
Onno Schuit <o.schuit@solin.nl>
2012-06-29 16:41:22 +0200

    Prepared frontend for archiving projects. Also added model category.

---
+++ public_html/mod/launcher/models/category.php
@@ -0,0 +1,18 @@
+<?php
+
+class category extends model {
+
+    static $table_name = 'course_categories';
+
+    // Overwrites parent's create function because we also need to create a context record.
+    function create() {
+        $category_id = parent::create();
+        // the unaptly named get_context_instance function will also create an new context record if no existing could be found
+        $context = get_context_instance(CONTEXT_COURSECAT, $category_id);
+        mark_context_dirty($context->path);
+        return $id;
+    } // function create
+
+} // class category 
+
+?>
